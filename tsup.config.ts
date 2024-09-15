d95677aefb7beaea2526d48cc8b24b6106c44a7c
Sebastian Pekarek <mail@sebbo.net>
2024-09-15 16:13:13 +0200

    chore(deps): Make all dependencies non-bundled

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'tsup';
+import fs from 'fs';
 
+const pkg = JSON.parse(
+    fs.readFileSync('./package.json', 'utf-8')
+);
 export default defineConfig({
     clean: true,
+    external: [
+        ...Object.keys(pkg.devDependencies),
+        ...Object.keys(pkg.dependencies),
+        'semantic-release'
+    ],
     entry: [
         'src/index.ts'
     ],
     format: ['esm', 'cjs'],
     dts: true,
+    shims: true,
     sourcemap: true,
     minify: true
 });
